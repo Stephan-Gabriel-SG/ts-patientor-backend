4498d66df7706204176433ef0ed75df8e3a25d3c
Stéphan Gabriel NANDRASANTSOA <gabrielstephannandrasantsoa@gmail.com>
2025-04-24 11:38:20 +0300

    feat: update patient service and types. adding gender type

--- src/services/patientServices.ts
+++ src/services/patientServices.ts
@@ -1,4 +1,10 @@
 import { Patient } from "./types";
 import patientData from "../data/patients";
-
+import { toNewPatient } from "./utils";
 export const getPatients = (): Patient[] => patientData;
+
+export const addPatient = (newPatient: unknown): Patient => {
+  const addedPatient = toNewPatient(newPatient);
+  patientData.push(addedPatient);
+  return addedPatient;
+};
