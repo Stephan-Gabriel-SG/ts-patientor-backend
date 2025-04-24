5fd074cf1d6972b5c8b0297addbf5ffc7c9b5d32
Stéphan Gabriel NANDRASANTSOA <gabrielstephannandrasantsoa@gmail.com>
2025-04-24 11:36:35 +0300

    feat: add post to create new patient

--- src/routes/patientRoute.ts
+++ src/routes/patientRoute.ts
@@ -1,9 +1,10 @@
 import express, { Response } from "express";
-import { getPatients } from "../services/patientServices";
+import { addPatient, getPatients } from "../services/patientServices";
 import { NonSensitivePatientInfo } from "../services/types";
-const route = express.Router();
 
-route.get("/", (_req, res: Response<NonSensitivePatientInfo[]>) => {
+const router = express.Router();
+
+router.get("/", (_req, res: Response<NonSensitivePatientInfo[]>) => {
   const patients = getPatients();
   res.status(200).json(
     patients.map((patient) => ({
@@ -16,4 +17,17 @@ route.get("/", (_req, res: Response<NonSensitivePatientInfo[]>) => {
   );
 });
 
-export default route;
+router.post("/", (req, res) => {
+  try {
+    const addedPatient = addPatient(req.body);
+    res.status(201).json(addedPatient);
+  } catch (error: unknown) {
+    let errorMessage = "Something went wrong.";
+    if (error instanceof Error) {
+      errorMessage += " Error: " + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
+});
+
+export default router;
