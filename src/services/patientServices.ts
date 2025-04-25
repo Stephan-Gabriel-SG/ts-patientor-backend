ce4fb2dbb0c422eaed01d10ccb76160523878023
Stéphan Gabriel NANDRASANTSOA <gabrielstephannandrasantsoa@gmail.com>
2025-04-25 10:26:54 +0300

    feat: refactoring type using zod and update patient Service

--- src/services/patientServices.ts
+++ src/services/patientServices.ts
@@ -1,10 +1,9 @@
 import { Patient } from "./types";
 import patientData from "../data/patients";
-import { toNewPatient } from "./utils";
+
 export const getPatients = (): Patient[] => patientData;
 
-export const addPatient = (newPatient: unknown): Patient => {
-  const addedPatient = toNewPatient(newPatient);
-  patientData.push(addedPatient);
-  return addedPatient;
+export const addPatient = (newPatient: Patient): Patient => {
+  patientData.push(newPatient);
+  return newPatient;
 };
--- src/services/types.ts
+++ src/services/types.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+
 export enum Gender {
   male = "male",
   female = "female",
@@ -10,13 +12,15 @@ export interface Diagnosis {
   latin?: string;
 }
 
-export interface Patient {
-  id: string;
-  name: string;
-  dateOfBirth: string;
-  ssn: string;
-  gender: Gender;
-  occupation: string;
-}
+export const newPatientSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  dateOfBirth: z.string().date(),
+  ssn: z.string(),
+  gender: z.nativeEnum(Gender),
+  occupation: z.string(),
+});
+
+export type Patient = z.infer<typeof newPatientSchema>;
 
 export type NonSensitivePatientInfo = Omit<Patient, "ssn">;
